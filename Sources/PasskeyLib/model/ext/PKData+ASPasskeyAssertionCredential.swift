66d9c6db81526fa94c77dd97417da43965b87bd5
eonist <30n1st@gmail.com>
2024-12-12 15:15:40 +0100

    Minor tweaks

--- Sources/PasskeyLib/model/ext/PKData+ASPasskeyAssertionCredential.swift
+++ Sources/PasskeyLib/model/ext/PKData+ASPasskeyAssertionCredential.swift
@@ -5,11 +5,6 @@ import AuthenticationServices
 extension PKData {
    /**
     * Generates an assertion credential using the provided client data hash and private key string.
-    *
-    * - Parameter clientDataHash: The hash of the client data used in the assertion.
-    * - Parameter privateKeyStr: The private key in string format used to sign the challenge.
-    * - Returns: An optional `ASPasskeyAssertionCredential` if the signature is successfully created; otherwise, `nil`.
-    *
     * **Example:**
     * ```swift
     * // Assume you have an instance of ASPasskeyAssertionCredential named `assertionCredential`
@@ -20,31 +15,34 @@ extension PKData {
     * } else {
     *     // Handle the error
     * }
-    * ```
+    * ``` 
+    * - Parameter clientDataHash: The hash of the client data used in the assertion.
+    * - Parameter privateKeyStr: The private key in string format used to sign the challenge.
+    * - Returns: An optional `ASPasskeyAssertionCredential` if the signature is successfully created; otherwise, `nil`.
     * - Fixme: ⚠️️ where do we get clientDataHash from? I think this is the credentialID data obj we get from external source
     */
-   public func getAssertionCredential(relyingParty: String, clientDataHash: Data, privateKeyStr: String) -> ASPasskeyAssertionCredential? {
+   public func getAssertionCredential(clientDataHash: Data, privateKeyStr: String) -> ASPasskeyAssertionCredential? {
       let authenticatorData: Data = authenticatorDataObj(relyingParty: relyingParty)
-        // Combine the authenticator data with the client data hash to form the challenge
-        let challenge = authenticatorData + clientDataHash
-        // Attempt to sign the challenge with the private key
+      // Combine the authenticator data with the client data hash to form the challenge
+      let challenge = authenticatorData + clientDataHash
+      // Attempt to sign the challenge with the private key
       guard let signature = PKSigner.signWithPrivateKey(challenge, privateKeyStr: privateKeyStr) else {
-            // Return nil if the signature could not be created
-            print("Signature could not be created")
-            return nil
-        }
+         // Return nil if the signature could not be created
+         print("Signature could not be created")
+         return nil
+      }
       guard let userHandleData = self.userHandleData else { print("err"); return nil }
       guard let credentialIDData = self.credentialIDData else { print("err"); return nil }
-        // Create an assertion credential with the necessary components
-        let assertion = ASPasskeyAssertionCredential(
+      // Create an assertion credential with the necessary components
+      let assertion = ASPasskeyAssertionCredential(
          userHandle: userHandleData, // Identifier for the user
-         relyingParty: self.relyingParty, // Identifier for the relying party
-            signature: signature, // The digital signature over the challenge
-            clientDataHash: clientDataHash, // The hash of the client data
-            authenticatorData: authenticatorData, // The authenticator data used in the challenge
+         relyingParty: relyingParty, // Identifier for the relying party
+         signature: signature, // The digital signature over the challenge
+         clientDataHash: clientDataHash, // The hash of the client data
+         authenticatorData: authenticatorData, // The authenticator data used in the challenge
          credentialID: credentialIDData // The identifier for the credential
-        )
-        // Return the newly created assertion credential
-        return assertion
-    }
+      )
+      // Return the newly created assertion credential
+      return assertion
+   }
 }
--- Sources/PasskeyLib/model/ext/PKData+ASPasskeyCredentialIdentity.swift
+++ Sources/PasskeyLib/model/ext/PKData+ASPasskeyCredentialIdentity.swift
@@ -8,9 +8,6 @@ extension PKData {
      * - Description: This method is typically used to convert raw passkey data along with a user identifier into a structured passkey credential identity.
      * - Parameters:
      *   - recordIdentifier: An optional unique identifier for the credential record, used to reference the credential in a database or storage system.
-     *   - username: The username associated with the credential. This is typically used for display purposes or during authentication processes.
-     *   - credentialID: A blob of data that uniquely identifies the credential. This is used by the authentication system to locate the credential.
-     *   - userHandle: A blob of data that represents the user handle. This is used to associate the credential with a specific user entity.
      * - Returns: An `ASPasskeyCredentialIdentity` instance populated with the provided data.
      * - Fixme: ⚠️️ this is missing some code, figure it out
      */
@@ -18,9 +15,8 @@ extension PKData {
        guard let userHandleData = self.userHandleData else { print("err"); return nil }
        guard let credentialIDData = self.credentialIDData else { print("err"); return nil }
         return .init(
-            // - Fixme: ⚠️️ get relyingParty from somewhere?
-            relyingPartyIdentifier: "", // The identifier for the relying party (e.g., a server) that the credential is associated with.
-            userName: self.username, // The username associated with the credential.
+            relyingPartyIdentifier: relyingParty, // The identifier for the relying party (e.g., a server) that the credential is associated with.
+            userName: username, // The username associated with the credential.
             credentialID: credentialIDData, // Unique identifier for the credential.
             userHandle: userHandleData, // Data representing the user handle.
             recordIdentifier: recordIdentifier // Optional unique identifier for the credential record.
